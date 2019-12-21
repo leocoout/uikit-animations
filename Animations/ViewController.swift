cff71cfab678bfa7d73cc9186bb46a7e3c6b72ea
Leonardo Coutinho <leonardo.santos@br.experian.com>
2019-12-20 22:46:53 -0300

    started project

---
+++ Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Leonardo Santos on 20/12/19.
+//  Copyright © 2019 Leonardo Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
